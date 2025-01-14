e6727c5505dd369bb7b72b09565138c7ef88ef38
leandro <leandro.desouzarosa@unibo.it>
2025-01-14 11:51:33 +0100

    start packing

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='peepholes',
+    version='0.0.1',
+    description='Peepholes extraction',
+    license='MIT',
+    packages=['src'],
+    author='Leandro de Souza Rosa',
+    author_email='leandro.desouzarosa@unibo.it',
+    keywords=['explainable AI, Attack detection, Confidence Estimation'],
+    url='https://github.com/SSIGPRO/XAI'
+)
---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['datasets', 'models', 'coreVectors', 'classifier', 'peepholes', 'utils']
