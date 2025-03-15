b6acf31fe1feffe1b3a1b56f60bef83b7b22b36f
lorenzo <lorenzo.capelli@outlook.com>
2025-03-15 17:04:08 +0100

    solving te score computation

--- src/xp_avg.py
+++ src/xp_avg.py
@@ -27,7 +27,7 @@ from cuda_selector import auto_cuda
 from tensordict import TensorDict
 from tensordict import MemoryMappedTensor as MMT
 
-def sample_estimator(n_classes, target_layers, act_dl, cv_dl, device):
+def sample_estimator(n_classes, target_layers, act_ds, cv_ds, device):
     """
     compute sample mean and precision (inverse of covariance)
     return: sample_class_mean: list of class mean
@@ -35,68 +35,140 @@ def sample_estimator(n_classes, target_layers, act_dl, cv_dl, device):
     """
     
     group_lasso = covariance.EmpiricalCovariance(assume_centered=False)
-    correct, total = 0, 0
     num_output = len(target_layers)
-    num_sample_per_class = np.empty(n_classes)
-    num_sample_per_class.fill(0)
+    num_sample_per_class = np.zeros(n_classes)
 #     _features = torch.zeros((n_classes, target_layers))
-    list_features = []
-    for i in range(num_output):
-        temp_list = []
+    list_features = {}
+    for key in target_layers:
+        list_features[key] = []
         for j in range(n_classes):
-            temp_list.append(0)
-        list_features.append(temp_list) ## it creats a list of dim num_output, n_classes
+            list_features[key].append(0)
+         ## it creats a list of dim num_output, n_classes
     
-    for acts, cvs in tqdm(zip(act_dl, cv_dl)): # we have a structure that correspond 
-                                               # to a dict whose keys are the layers 
-                                               # in target_layers and zip with acivation
+    for i, (act, cv) in tqdm(enumerate(zip(act_ds, cv_ds))): # we have a structure that correspond 
+                                                            # to a dict whose keys are the layers 
+                                                             # in target_layers and zip with acivation
+            
+        label = int(act['label']) 
         
-        # construct the sample matrix
-        for act, cv in zip(acts, cvs): # they iterate over the batch
+        if num_sample_per_class[label] == 0:
+            # out_count = 0
+            for layer in target_layers:## it iterates over the layer
+                list_features[layer][label] = cv[layer].view(1, -1)
+                # out_count += 1
+        else:
+            # out_count = 0
+            for layer in target_layers:
+                list_features[layer][label] \
+                = torch.cat((list_features[layer][label], cv[layer].view(1, -1)), 0)
+                # out_count += 1                
+        num_sample_per_class[label] += 1
             
-            label = int(act['label']) 
-            
-            if num_sample_per_class[label] == 0:
-                out_count = 0
-                for i, layer in enumerate(target_layers):## it iterates over the layer
-                    list_features[out_count][label] = cv[layer][i].view(1, -1)
-                    out_count += 1
-            else:
-                out_count = 0
-                for i, layer in enumerate(target_layers):
-                    list_features[out_count][label] \
-                    = torch.cat((list_features[out_count][label], cv[layer][i].view(1, -1)), 0)
-                    out_count += 1                
-            num_sample_per_class[label] += 1
-            
-    sample_class_mean = []
-    out_count = 0
-    for i, (layer, data) in enumerate(cv_dl.dataset.items()):
-        _size = data.size(1)
+    sample_class_mean = {}
+    # out_count = 0
+    for layer in target_layers:
+        _size = cv_ds[layer].size(1)
         temp_list = torch.Tensor(n_classes, _size).cuda()
         for j in range(n_classes):
-            temp_list[j] = torch.mean(list_features[out_count][j], 0)
-        sample_class_mean.append(temp_list)
-        out_count += 1
-    precision = []
-    for k in range(num_output):
+            temp_list[j] = torch.mean(list_features[layer][j], 0)
+        sample_class_mean[layer] = temp_list
+        # out_count += 1
+    precision = {}
+    for layer in target_layers:
         X = 0
         for i in range(n_classes):
             if i == 0:
-                X = list_features[k][i].to(device) - sample_class_mean[k][i].to(device)
+                X = list_features[layer][i].to(device) - sample_class_mean[layer][i].to(device)
                 print(X)
 
             else:
-                X = torch.cat((X, list_features[k][i].to(device) - sample_class_mean[k][i]), 0)       
+                X = torch.cat((X, list_features[layer][i].to(device) - sample_class_mean[layer][i]), 0)       
         # find inverse            
         group_lasso.fit(X.cpu().numpy())
         temp_precision = group_lasso.precision_
         temp_precision = torch.from_numpy(temp_precision).float().cuda()
-        precision.append(temp_precision)
-        
-    print('\n Training Accuracy:({:.2f}%)\n'.format(100. * correct / total))
+        precision[layer] = temp_precision
+
 
     return sample_class_mean, precision
+
+def get_Mahalanobis_score(model, act_dl, cv_dl, num_classes, outf, out_flag, net_type, sample_mean, precision, layer, magnitude):
+    '''
+    Compute the proposed Mahalanobis confidence score on input dataset
+    return: Mahalanobis score from layer_index It computes the score for a single layer at the time
+
+
+    sample_mean is a DICT of a number of elemnts that is equal to the number of layers present in target layers. Each element is a tensor 
+    of dim (n_classes, dim of the coreavg) in layer features.28 it will be(100,512)
+    precision is a DICT of precision matrices one for each layer
+    '''
+    Mahalanobis = []
+    
+    if out_flag == True:
+        temp_file_name = '%s/confidence_Ga%s_In.txt'%(outf, str(layer))
+    else:
+        temp_file_name = '%s/confidence_Ga%s_Out.txt'%(outf, str(layer))
+        
+    g = open(temp_file_name, 'w')
+    
+    for acts, cvs in tqdm(zip(act_dl, cv_dl)):
+
+        data = acts['image']
+        target = acts['label']
+        out_features = cvs[layer]
+        
+        # compute Mahalanobis score
+        gaussian_score = 0
+        for i in range(num_classes):
+            batch_sample_mean = sample_mean[layer][i]
+            zero_f = out_features.data - batch_sample_mean
+            term_gau = -0.5*torch.mm(torch.mm(zero_f, precision[layer]), zero_f.t()).diag()
+            if i == 0:
+                gaussian_score = term_gau.view(-1,1)
+            else:
+                gaussian_score = torch.cat((gaussian_score, term_gau.view(-1,1)), 1)
+        
+        # Input_processing
+        sample_pred = gaussian_score.max(1)[1]
+        batch_sample_mean = sample_mean[layer].index_select(0, sample_pred)
+        zero_f = out_features - batch_sample_mean
+        pure_gau = -0.5*torch.mm(torch.mm(zero_f, precision[layer]), zero_f.t()).diag()
+        loss = torch.mean(-pure_gau)
+        loss.backward()
+         
+        gradient =  torch.ge(data.grad.data, 0)
+        gradient = (gradient.float() - 0.5) * 2
+        if net_type == 'densenet':
+            gradient.index_copy_(1, torch.LongTensor([0]).cuda(), gradient.index_select(1, torch.LongTensor([0]).cuda()) / (63.0/255.0))
+            gradient.index_copy_(1, torch.LongTensor([1]).cuda(), gradient.index_select(1, torch.LongTensor([1]).cuda()) / (62.1/255.0))
+            gradient.index_copy_(1, torch.LongTensor([2]).cuda(), gradient.index_select(1, torch.LongTensor([2]).cuda()) / (66.7/255.0))
+        elif net_type == 'resnet':
+            gradient.index_copy_(1, torch.LongTensor([0]).cuda(), gradient.index_select(1, torch.LongTensor([0]).cuda()) / (0.2023))
+            gradient.index_copy_(1, torch.LongTensor([1]).cuda(), gradient.index_select(1, torch.LongTensor([1]).cuda()) / (0.1994))
+            gradient.index_copy_(1, torch.LongTensor([2]).cuda(), gradient.index_select(1, torch.LongTensor([2]).cuda()) / (0.2010))
+        tempInputs = torch.add(data.data, -magnitude, gradient)
+ 
+        noise_out_features = model.intermediate_forward(Variable(tempInputs, volatile=True), layer_index)
+        noise_out_features = noise_out_features.view(noise_out_features.size(0), noise_out_features.size(1), -1)
+        noise_out_features = torch.mean(noise_out_features, 2)
+        noise_gaussian_score = 0
+        for i in range(num_classes):
+            batch_sample_mean = sample_mean[layer_index][i]
+            zero_f = noise_out_features.data - batch_sample_mean
+            term_gau = -0.5*torch.mm(torch.mm(zero_f, precision[layer_index]), zero_f.t()).diag()
+            if i == 0:
+                noise_gaussian_score = term_gau.view(-1,1)
+            else:
+                noise_gaussian_score = torch.cat((noise_gaussian_score, term_gau.view(-1,1)), 1)      
+
+        noise_gaussian_score, _ = torch.max(noise_gaussian_score, dim=1)
+        Mahalanobis.extend(noise_gaussian_score.cpu().numpy())
+        
+        for i in range(data.size(0)):
+            g.write("{}\n".format(noise_gaussian_score[i]))
+    g.close()
+
+    return Mahalanobis
 
 
 if __name__ == "__main__":
@@ -256,7 +328,7 @@ if __name__ == "__main__":
                  )
         sample_class_mean, precision = sample_estimator(n_classes=n_classes, 
                                                         target_layers=target_layers, 
-                                                        act_dl=act_dl['train'], 
-                                                        cv_dl=cv_dl['train'],
+                                                        act_ds=act_dl['train'].dataset, 
+                                                        cv_ds=cv_dl['train'].dataset,
                                                         device=device)
-        print(sample_class_mean, precision)
+    
