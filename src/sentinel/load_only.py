edba4c5f14a92cc3e5e91367b6f7e428f9d34560
Elma <eplava4@gmail.com>
2025-10-02 14:59:48 +0200

    comp empp

--- src/sentinel/load_only.py
+++ src/sentinel/load_only.py
@@ -16,6 +16,8 @@ from peepholelib.datasets.sentinel import SentinelWrap
 from peepholelib.models.model_wrap import ModelWrap
 from peepholelib.models.svd_fns import conv2d_toeplitz_svd, linear_svd
 
+
+from peepholelib.peepholes.peepholes import Peepholes
 from peepholelib.peepholes.classifiers.tgmm import GMM as tGMM
 from peepholelib.peepholes.parsers import trim_corevectors
 from peepholelib.coreVectors.coreVectors import CoreVectors
@@ -37,6 +39,10 @@ if __name__ == "__main__":
     cvs_path = Path.cwd()/'../../data/corevectors'
     cvs_name = 'cvs'
 
+    
+    phs_path = Path.cwd()/'../../data/peepholes'
+    phs_name = 'peepholes'
+
     drill_path = Path.cwd()/'../../data/drillers'
     drill_name = 'drills'
 
@@ -44,7 +50,9 @@ if __name__ == "__main__":
     bs = 1024*5*10
     verbose = True 
     input_key = 'data'
+    n_threads = 1
     n_cluster = 5
+    n_classes = 5#'corruptions' #this is the number of clusters
     #model = "ae1Dregn16_2_ns0_k001.pth"
 
     model_dir = '/srv/newpenny/XAI/EP/Model_ready/ae1Dregn16_2_ns0_k001.pth'
@@ -74,10 +82,11 @@ if __name__ == "__main__":
     
     with sentinel as s:
         s.load_only(
-            loaders = ['val', 'val-c'],
+            loaders = ['val-c'],#val-c
             verbose = verbose
         )
-        
+        #print(s._dss['val']['output'])
+        #quit()
         svd_fns = {
             'encoder.linear': partial(
             linear_svd, 
@@ -94,7 +103,7 @@ if __name__ == "__main__":
                 path = svds_path,
                 name = svds_name,
                 target_modules = target_layers,
-                sample_in = sentinel._dss['train'][0]['data'],
+                sample_in = sentinel._dss['val-c'][0]['data'],
                 svd_fns = svd_fns,
                 verbose = verbose
                 )
@@ -136,13 +145,13 @@ if __name__ == "__main__":
 
     with sentinel as s, cvs as cv:
         s.load_only(
-            loaders = loaders,
+            loaders = ['val-c'],#loaders,
             verbose = verbose
         )
         
         cv.get_coreVectors(
             datasets = s,#sentinel,
-            loaders = loaders,
+            loaders = ['val-c'],#loaders,
             input_key = input_key,
             reduction_fns = reduction_fns,
             batch_size = bs,
@@ -152,7 +161,7 @@ if __name__ == "__main__":
         
         if not (cvs_path/(cvs_name+'.normalization.pt')).exists():
             cv.normalize_corevectors(
-                    wrt = 'train',#sentinel._dss['train'][0:1][input_key],
+                    wrt = 'val-c',#sentinel._dss['train'][0:1][input_key], val-c only to test: was val
                     to_file = cvs_path/(cvs_name+'.normalization.pt'),
                     #from_file = cvs_path/(cvs_name+'.normalization.pt'),
                     #loaders = ['CIFAR100-val', 'CIFAR100-test'],
@@ -166,12 +175,13 @@ if __name__ == "__main__":
                 print('cv shapes: ', k, kk, cv._corevds[k][kk].shape)
             
             v = cv._corevds[k]#['encoder.linear']
-            print(f'v={v}')
-            fig = plt.figure()
+            #plotting the cv just to see how looks like
+            #print(f'v={v}')
+            #fig = plt.figure()
             # plot core vector 1
-            plt.plot(v['encoder.linear'][0])
-            plt.savefig('linear_l_cv_trial2.png')
-            plt.close()
+            #plt.plot(v['encoder.linear'][0])
+            #plt.savefig('linear_l_cv_trial2.png')
+            #plt.close()
         
             '''
             fig = plt.figure()
@@ -183,12 +193,13 @@ if __name__ == "__main__":
     # drillers 
     #TODO see how GMM works from the name seems gaussian mixture  
     #TODO drillers peepholesthis will be done after implementation of labels
-
+    # put thhis somewhere :label_key = corruptions
     cv_parsers = {
         'encoder.linear': partial(
             trim_corevectors,
             module = 'encoder.linear',
-            cv_dim = layer_cv_dim
+            cv_dim = layer_cv_dim,
+            label_key = 'corruption'
         )
     }
 
@@ -207,4 +218,96 @@ if __name__ == "__main__":
                 parser = cv_parsers[peep_layer],
                 device = device
                 )
- 
+    
+    peepholes = Peepholes(
+            path = phs_path,
+            name = phs_name,
+            device = device
+            )
+    
+    # fitting the classifiers
+    with sentinel as s, cvs as cv:
+        s.load_only(
+            loaders = ['val-c'],
+            verbose = verbose
+        )
+
+        cv.load_only(
+            loaders = ['val-c'],
+            verbose = verbose
+        )
+
+        for drill_key, driller in drillers.items():
+            if (driller._empp_file).exists():
+                print(f'Loading Classifier for {drill_key}') 
+                v = driller.load()
+                #print(f'driller_key={driller}')
+
+            else:
+                t0 = time()
+                print(f'Fitting classifier for {drill_key}')
+                driller.fit(
+                        corevectors = cv,
+                        loader = 'val-c',
+                        verbose=verbose
+                        )
+                print(f'Fitting time for {drill_key}  = ', time()-t0)
+                print(f'driller={drill_key}{driller}')
+
+                driller.compute_empirical_posteriors(
+                        datasets = s,
+                        corevectors = cv,
+                        loader = 'val-c',
+                        batch_size = bs,
+                        verbose=verbose
+                        )
+        
+                # save classifiers
+                print(f'Saving classifier for {drill_key}')
+                driller.save()
+                #plt.imshow(driller.empp)
+                
+            #fig = plt.figure()
+            #plt.plot(v)
+            #p#lt.savefig('driller.png')
+            #plt.close()
+            #since _emmp is tensor to visualize needed to convert to numpy 1st
+            #TODO find a way to plot on vscode and not save as img
+            d = driller._empp.detach().cpu().numpy()
+            plt.imsave('empp.png',d)
+
+    # compute the peepholes
+    with sentinel as s, cvs as cv, peepholes as ph:
+        s.load_only(
+                loaders = loaders,
+                verbose = verbose
+                )
+
+        cv.load_only(
+                loaders = loaders,
+                verbose = verbose 
+                ) 
+
+        ph.get_peepholes(
+                datasets = s,
+                corevectors = cv,
+                target_modules = target_layers,
+                batch_size = bs,
+                drillers = drillers,
+                n_threads = n_threads,
+                verbose = verbose
+            )
+        
+        
+
+        for k in ph._phs.keys():
+            for kk in ph._phs[k].keys():
+                print('ph shapes: ', k, kk, ph._phs[k][kk].shape)
+            
+            peeps = ph._phs[k]#['encoder.linear']
+            #print(peeps)
+            # plots nthgggggggggggg
+            fig = plt.figure()
+            plt.plot(peeps['encoder.linear']['peepholes'].cpu())
+            plt.savefig('peephole_trial.png')
+            plt.close()
