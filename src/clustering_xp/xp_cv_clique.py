405ac4c27824110f770ba49f5a2d7c82e1cc6772
clrnb <clara.barrancos@hotmail.com>
2025-12-05 10:18:07 +0100

    some extra debug on clique

--- src/clustering_xp/xp_cv_clique.py
+++ src/clustering_xp/xp_cv_clique.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path as Path
 sys.path.insert(0, (Path.home()/'repos/peepholelib').as_posix())
+sys.path.append(Path.home()/'repos/XAI/src/clustering_xp/Clique')
 
 # torch stuff
 import torch
@@ -10,7 +11,7 @@ from cuda_selector import auto_cuda
 ###### Our stuff
 from peepholelib.models.model_wrap import ModelWrap 
 from peepholelib.datasets.cifar100 import Cifar100
-
+from peepholelib.utils.viz_tsne import plot_tsne
 from peepholelib.datasets.parsedDataset import ParsedDataset 
 
 
@@ -19,8 +20,11 @@ from peepholelib.coreVectors.coreVectors import CoreVectors
 from peepholelib.coreVectors.dimReduction.svds import linear_svd_projection, conv2d_toeplitz_svd_projection
 
 import numpy as np
+import re
+from collections import Counter
+from itertools import combinations
 
-from pyclustering.cluster.clique import clique
+from Clique.Clique import  run_clique, normalize_features, evaluate_clustering_performance, save_to_file, plot_clusters
 
 
 def compute_empp_clique(hard_labels, y, n_classes):
@@ -66,7 +70,42 @@ def compute_empp_clique(hard_labels, y, n_classes):
 
     return empp
 
+def get_cluster_count(filepath):
+    single_counts = Counter()
+    pair_counts = Counter()
 
+    filepath = "/home/claranunesbarrancos/repos/XAI/src/clustering_xp/clique_clusters/clusters_xsi_10_tau_01_layer_classifier.6.csv"
+
+    with open(filepath, "r") as f:
+        for line in f:
+            match = re.search(r"Dimensions:\s*\{([^}]*)\}", line)
+            if match:
+                dims = match.group(1)
+                
+                dims = [int(d.strip()) for d in dims.split(",") if d.strip().isdigit()]
+
+                # 1D clusters
+                if len(dims) == 1:
+                    single_counts[dims[0]] += 1
+
+                # 2D clusters (usually never goes beyond this)
+                elif len(dims) == 2:
+                    pair = tuple(sorted(dims))
+                    pair_counts[pair] += 1
+
+    print("\n=== Single-dimension counts ===")
+    print(single_counts)
+    if single_counts:
+        most_common_single = single_counts.most_common(1)[0]
+        print(f"Most common single dimension: {most_common_single[0]} (count={most_common_single[1]})")
+
+    print("\n=== 2D pair counts ===")
+    print(pair_counts)
+    if pair_counts:
+        most_common_pair = pair_counts.most_common(1)[0]
+        print(f"Most common pair: {most_common_pair[0]} (count={most_common_pair[1]})")
+
+    quit()
 
 if __name__ == "__main__":
 
@@ -84,13 +123,13 @@ if __name__ == "__main__":
     cvs_path = Path.cwd()/'../data/corevectors'
     cvs_name = 'corevectors'
 
-    plots_path = Path.cwd()/'temp_plots/coverage/'
+    plots_path = Path.cwd()/'temp_plots'
 
     target_layers = [
-        # 'features.7', 'features.10', 'features.12', 'features.14', 'features.17',
+         'features.7', #'features.10', 'features.12', 'features.14', 'features.17',
         # 'features.19', 'features.21', 'features.24', 'features.26', 'features.28',
         # 'classifier.0', 'classifier.3',
-        'classifier.6',
+        #'classifier.6',
     ]
 
     loaders = ['CIFAR100-train', 'CIFAR100-val', 'CIFAR100-test']
@@ -135,37 +174,43 @@ if __name__ == "__main__":
             y = ds._dss["CIFAR100-train"][:]["label"]  
             y_np = y.cpu().numpy()
 
+            plot_tsne(X_np = X_np, 
+                save_path = plots_path,
+                file_name = "classifier6_tsne")
+            quit()
+
             # -----------------------------
             # CLIQUE 
             # -----------------------------
-
-            clique = clique(
-                    data = X_np,
-                    amount_intervals = 10, # grid bins per dimension.
-                    density_threshold = 10 # min points required in a grid cell for it to be considered dense
+            xsi = 15  # number of grid intervals per dimension
+            tau = 0.01  # is dense if number_of_points_in_cell > tau
+            clusters = run_clique(
+                data=X_np,
+                xsi=xsi, 
+                tau=tau, 
             )
+            print(f"\nFound {len(clusters)} clusters\n")
+            save_to_file(clusters, Path("../clique_clusters")/f'clusters_xsi_{xsi}_tau_{tau}_layer_{layer}.csv')
 
-            clique.process()
-            clusters = clique.get_clusters() 
-            noise = clique.get_noise()
+            print("\n=== Evaluation for layer:", layer, "===")
+            evaluate_clustering_performance(clusters, y_np)
 
-            pred = np.full(len(X_np), -1, dtype=int)
-            for cid, pts in enumerate(clusters):
-                pred[np.array(pts, dtype=int)] = cid
+            print("ploting cluster for layer ", layer)
+            title = ("DS: " + "clique_cluster.csv" + " - Params: Tau=" + str(tau) + " Xsi=" + str(xsi))
+            plot_clusters(data=X_np, clusters=clusters, title=title, xsi=xsi,
+                          save_path=plots_path, filename=f'clique_clusters_xsi_{xsi}_tau_{tau}_layer_{layer}.png')
 
-            pred_labels = torch.tensor(pred, dtype=torch.long)
+            # empp = compute_empp_clique(
+            #     hard_labels=pred_labels,
+            #     y=y_np,
+            #     n_classes=n_classes
+            # )
 
-            empp = compute_empp_clique(
-                hard_labels=pred_labels,
-                y=y_np,
-                n_classes=n_classes
-            )
+            # drillers[layer] = {
+            #     "_empp": empp
+            # }
 
-            drillers[layer] = {
-                "_empp": empp
-            }
-
-            # just to see whatsup
-            unique, counts = torch.unique(pred_labels, return_counts=True)
-            for u, c in zip(unique.tolist(), counts.tolist()):
-                print(f"Cluster {u}: {c} points")
+            # # just to see whatsup
+            # unique, counts = torch.unique(pred_labels, return_counts=True)
+            # for u, c in zip(unique.tolist(), counts.tolist()):
+            #     print(f"Cluster {u}: {c} points")
