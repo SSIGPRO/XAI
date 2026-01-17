00193e38b6c23500f15a2689eddb2e72c3d7fa79
leandro <leandro.desouzarosa@unibo.it>
2026-01-17 09:59:56 +0100

    minor fix

--- src/conv_red/configs/maxpooling.py
+++ src/conv_red/configs/maxpooling.py
@@ -4,6 +4,7 @@ cvs_name = 'maxpooling'
 
 # function to compute reducers's extra kwargs
 def get_reducer_kwargs(tl):
+    ret = {}
     for _n, _l in tl.items():
         ret[_n] = {}
     return ret
