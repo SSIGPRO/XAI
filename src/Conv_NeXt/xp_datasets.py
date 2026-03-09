02298a8624013c9e95b9907f38f372a1169167f3
Arsharathi1 <arsharathi56@gmail.com>
2026-03-09 12:14:19 +0100

    backup convnext

---
+++ src/Conv_NeXt/xp_datasets.py
@@ -0,0 +1,186 @@
+import sys
+from pathlib import Path as Path
+sys.path.insert(0, (Path.home()/'repos/peepholelib').as_posix())
+
+# python stuff
+from functools import partial
+from matplotlib import pyplot as plt
+
+# torch stuff
+import torch
+from torchvision.models import vgg16
+from cuda_selector import auto_cuda
+
+###### Our stuff
+
+# Model
+from peepholelib.models.model_wrap import ModelWrap 
+
+# datasets
+from peepholelib.datasets.cifar100 import Cifar100
+from peepholelib.datasets.cifarC import CifarC
+from peepholelib.datasets.parsedDataset import ParsedDataset 
+
+from peepholelib.datasets.functional.inference_fns import img_classification_full as img_cls_inf, img_classification_atks as img_cls_atk_inf 
+from peepholelib.datasets.functional.transforms import TransformWrap 
+from peepholelib.datasets.functional.transforms import vgg16_transform as ds_transform 
+from peepholelib.datasets.functional.samplers import random_subsampling 
+
+# ATK dataset
+from peepholelib.adv_atk.BIM import myBIM
+from peepholelib.adv_atk.CW import myCW
+from peepholelib.adv_atk.DeepFool import myDeepFool
+from peepholelib.adv_atk.PGD import myPGD
+
+if __name__ == "__main__":
+    use_cuda = torch.cuda.is_available()
+    device = torch.device(auto_cuda('memory')) if use_cuda else torch.device("cpu")
+    print(f"Using {device} device")
+
+    #--------------------------------
+    # Directories definitions
+    #--------------------------------
+    cifar_path = '/srv/newpenny/dataset/CIFAR100'
+    cifarc_path = '/srv/newpenny/dataset/CIFAR-100-C'
+    ds_path = Path.cwd()/'../data/datasets'
+    atk_path = '../data/attacks/'
+
+    # model parameters
+    seed = 29
+    
+    bs = 2**8
+    n_threads = 1
+
+    model_dir = '/srv/newpenny/XAI/models'
+    model_name = 'LM_model=vgg16_dataset=CIFAR100_augment=True_optim=SGD_scheduler=LROnPlateau.pth'
+     
+    verbose = True 
+    
+    #--------------------------------
+    # Model 
+    #--------------------------------
+    nn = vgg16()
+    n_classes = 100#len(ds.get_classes()) 
+    model = ModelWrap(
+            model = nn,
+            device = device
+            )
+                                            
+    model.update_output(
+            output_layer = 'classifier.6', 
+            to_n_classes = n_classes,
+            overwrite = True 
+            )
+                                            
+    model.load_checkpoint(
+            name = model_name,
+            path = model_dir,
+            verbose = verbose
+            )
+                                            
+    #--------------------------------
+    # Datasets 
+    #--------------------------------
+    # original datasets
+    _dss = {
+            'CIFAR100': Cifar100(
+                path = cifar_path,
+                seed = seed
+                ),
+            'CIFARC': CifarC(
+                path = cifarc_path,
+                seed = seed
+                )
+            }
+
+    _dss_samplers = {
+            k: partial(
+                random_subsampling, 
+                perc = 0.5
+                ) for k in _dss.keys()
+            }
+
+    loaders = [
+            'CIFAR100-train',
+            'CIFAR100-test',
+            'CIFAR100-val',
+            'CIFAR100-C-train-c0',
+            'CIFAR100-C-val-c0',
+            'CIFAR100-C-test-c0',
+            'CIFAR100-C-train-c1',
+            'CIFAR100-C-val-c1',
+            'CIFAR100-C-test-c1',
+            'CIFAR100-C-train-c2',
+            'CIFAR100-C-val-c2',
+            'CIFAR100-C-test-c2',
+            'CIFAR100-C-train-c3',
+            'CIFAR100-C-val-c3',
+            'CIFAR100-C-test-c3',
+            'CIFAR100-C-train-c4',
+            'CIFAR100-C-val-c4',
+            'CIFAR100-C-test-c4',
+            ]
+
+    _transforms = {
+            k: TransformWrap(transform=ds_transform, input_key='image') for k in loaders 
+            }
+
+    atks = {
+            'CW': myCW(
+                model = model,
+                max_steps = 10,
+                ),
+            'BIM': myBIM(
+                model = model,
+                ),
+            'DF': myDeepFool(
+                model = model,
+                ),
+            'PGD': myPGD(
+                model = model,
+                ),
+            }
+
+    # create inference functions for each atk
+    atks_inf_fns = {
+            atk_name: partial(
+                img_cls_atk_inf,
+                attack = atk,
+                label_key = 'label'
+                ) for atk_name, atk in atks.items()
+            }
+
+
+    #######################
+    # parsing datasets
+    #######################
+    dataset = ParsedDataset(
+            path = ds_path
+            )
+
+    with dataset as ds:
+        ds.parse_dataset(
+                dataset_wraps = _dss,
+                ds_samplers = _dss_samplers, 
+                keys_to_copy = ['image', 'label'],
+                batch_size = bs,
+                n_threads = 1,
+                verbose = verbose
+                ) 
+
+        ds.parse_inference(
+                inference_fns = {'vgg': partial(img_cls_inf, model=model)},
+                transforms = _transforms,
+                batch_size = bs,
+                n_threads = 1,
+                verbose = verbose
+                )
+
+        ds.parse_inference(
+                loaders = ['CIFAR100-test-vgg'],
+                inference_fns = atks_inf_fns, 
+                transforms = _transforms,
+                batch_size = bs,
+                verbose = verbose 
+                )
+
---
+++ src/Conv_NeXt/xp_ph_for_convnext_small.py
@@ -0,0 +1,288 @@
+import sys
+from pathlib import Path as Path
+sys.path.insert(0, (Path.home()/'repos/peepholelib').as_posix())
+
+# python stuff
+from time import time
+from functools import partial
+
+# torch stuff
+import torch
+from torchvision.models import convnext_small
+from cuda_selector import auto_cuda
+
+###### Our stuff
+
+# Model
+from peepholelib.models.model_wrap import ModelWrap 
+
+# datasets
+from peepholelib.datasets.cifar100 import Cifar100
+from peepholelib.datasets.parsedDataset import ParsedDataset
+from peepholelib.datasets.functional.transforms import TransformWrap 
+from peepholelib.datasets.functional.transforms import convnext_small_transform as ds_transform 
+
+# corevecs
+from peepholelib.coreVectors.coreVectors import CoreVectors
+from peepholelib.coreVectors.dimReduction.svds.linear_svd import LinearSVD
+from peepholelib.coreVectors.dimReduction.svds.conv2d_toeplitz_svd import Conv2dToeplitzSVD
+from peepholelib.coreVectors.dimReduction.svds.conv2d_avg_kernel_svd import Conv2dAvgKernelSVD
+
+# peepholes
+from peepholelib.peepholes.classifiers.tgmm import GMM as tGMM 
+from peepholelib.peepholes.peepholes import Peepholes
+
+if __name__ == "__main__":
+    use_cuda = torch.cuda.is_available()
+    device = torch.device(auto_cuda('memory')) if use_cuda else torch.device("cpu")
+    print(f"Using {device} device")
+
+    #--------------------------------
+    # Directories definitions
+    #--------------------------------
+    cifar_path = '/srv/newpenny/dataset/CIFAR100'
+    ds_path = Path.cwd()/'../data/datasets'
+
+    # model parameters
+    bs = 512 
+    n_threads = 1
+
+    model_path = '/srv/newpenny/XAI/models'
+    model_name = 'convnext_cifar100_clean_sd.pt'
+     
+    svds_path = Path.cwd()/'../data/svds'
+    
+    cvs_path = Path.cwd()/'../data/corevectors'
+    cvs_name = 'corevectors'
+
+    drill_path = Path.cwd()/'../data/drillers'
+    drill_name = 'classifier'
+
+    phs_path = Path.cwd()/'../data/peepholes'
+    phs_name = 'peepholes'
+    
+    verbose = True 
+    
+    # Peepholelib
+    target_layers = [
+            'features.26',
+            'features.28',
+            'classifier.0',
+            ]
+    
+    cv_dims = {
+            'features.26': 30,
+            'features.28': 50,
+            'classifier.0': 30,
+            }
+
+    svd_rank = 300
+    n_cluster = 4 
+    
+    loaders = [
+            'CIFAR100-train',
+            'CIFAR100-val',
+            'CIFAR100-test',  
+            'CIFAR100-C-val-c0',
+            'CIFAR100-C-test-c0' 
+            ]
+
+    _transforms = {
+            k: TransformWrap(transform=ds_transform, input_key='image') for k in loaders 
+            }
+
+    _inference_names = {
+            k: ['vgg'] for k in loaders
+            }
+
+    n_classes = len(Cifar100.get_classes(meta_path = Path(cifar_path)/'cifar-100-python/meta')) 
+
+    #--------------------------------
+    # Model 
+    #--------------------------------
+
+    model = ModelWrap(
+            model = vgg16(),
+            target_modules = target_layers,
+            device = device
+            )
+                                            
+    model.update_output(
+            output_layer = 'classifier.6', 
+            to_n_classes = n_classes,
+            overwrite = True 
+            )
+                                            
+    model.load_checkpoint(
+            name = model_name,
+            path = model_path,
+            verbose = verbose
+            )
+                                            
+    #--------------------------------
+    # Datasets 
+    #--------------------------------
+    
+    # Assuming we have a parsed dataset in ds_path
+    datasets = ParsedDataset(
+            path = ds_path,
+            )
+
+    #--------------------------------
+    # SVDs 
+    #--------------------------------
+    t0 = time()
+    with datasets as ds:
+        ds.load_only(
+                loaders = ['CIFAR100-train'],
+                transforms = _transforms,
+                inference_names = _inference_names,
+                verbose = verbose
+                )
+        sample_in = ds._dss['CIFAR100-train-vgg'][0]['image']
+
+        svds = {
+                'features.26': Conv2dToeplitzSVD(
+                    path = svds_path,
+                    layer = 'features.26',
+                    model = model,
+                    rank = svd_rank,
+                    cv_dim = cv_dims['features.26'],
+                    sample_in = sample_in,
+                    ),
+                'features.28': Conv2dAvgKernelSVD(
+                    path = svds_path,
+                    layer = 'features.28',
+                    model = model,
+                    rank = svd_rank,
+                    cv_dim = cv_dims['features.28'],
+                    ),
+                'classifier.0': LinearSVD(
+                    path = svds_path,
+                    layer = 'classifier.0',
+                    model = model,
+                    rank = svd_rank,
+                    cv_dim = cv_dims['classifier.0'],
+                    verbose = verbose
+                    ),
+                }
+    print('time: ', time()-t0)
+
+    #--------------------------------
+    # CoreVectors 
+    #--------------------------------
+    
+    corevecs = CoreVectors(
+            path = cvs_path,
+            name = cvs_name,
+            model = model,
+            )
+    
+    with datasets as ds, corevecs as cv: 
+        ds.load_only(
+                loaders = loaders,
+                transforms = _transforms,
+                inference_names = _inference_names,
+                verbose = verbose
+                )
+
+        # computing the corevectors
+        cv.get_coreVectors(
+                datasets = ds,
+                reducers = svds,
+                save_input = True,
+                save_output = False,
+                batch_size = bs,
+                n_threads = n_threads,
+                verbose = verbose
+                )
+
+        if not (cvs_path/(cvs_name+'.normalization.pt')).exists():
+            cv.normalize_corevectors(
+                    wrt = 'CIFAR100-train-vgg',
+                    to_file = cvs_path/(cvs_name+'.normalization.pt'),
+                    #from_file = cvs_path/(cvs_name+'.normalization.pt'),
+                    #loaders = ['CIFAR100-val', 'CIFAR100-test'],
+                    batch_size = bs,
+                    n_threads = n_threads,
+                    verbose=verbose
+                    )
+
+    #--------------------------------
+    # Peepholes
+    #--------------------------------
+    drillers = {}
+    for peep_layer in target_layers:
+        drillers[peep_layer] = tGMM(
+                path = drill_path,
+                name = f'{drill_name}.GMM.{peep_layer}.{n_classes}.{cv_dims[peep_layer]}.{n_cluster}',
+                target_module = peep_layer,
+                nl_classifier = n_cluster,
+                nl_model = n_classes,
+                n_features = cv_dims[peep_layer],
+                cls_kwargs = {
+                    'covariance_regularization': 1e-4,
+                    'convergence_tolerance': 1e-2
+                    },
+                reducer = svds[peep_layer],
+                device = device
+                )
+
+    peepholes = Peepholes(
+            path = phs_path,
+            name = phs_name,
+            device = device
+            )
+
+    # fitting classifiers
+    with datasets as ds, corevecs as cv:
+        ds.load_only(
+                loaders = loaders,
+                transforms = _transforms,
+                inference_names = _inference_names,
+                verbose = verbose
+                )
+
+        cv.load_only(
+                loaders = list(ds._dss.keys()),
+                verbose = verbose 
+                ) 
+
+        for drill_key, driller in drillers.items():
+            if not driller.load():
+                t0 = time()
+                print(f'Fitting classifier for {drill_key}')
+                driller.fit(
+                        datasets = ds,
+                        corevectors = cv,
+                        loader = 'CIFAR100-train-vgg',
+                        verbose=verbose
+                        )
+                print(f'Fitting time for {drill_key}  = ', time()-t0)
+
+                # save classifiers
+                print(f'Saving classifier for {drill_key}')
+                driller.save()
+
+    with datasets as ds, corevecs as cv, peepholes as ph:
+        ds.load_only(
+                loaders = loaders,
+                transforms = _transforms,
+                inference_names = _inference_names,
+                verbose = verbose
+                )
+
+        cv.load_only(
+                loaders = list(ds._dss.keys()),
+                verbose = verbose 
+                ) 
+
+        ph.get_peepholes(
+                datasets = ds,
+                corevectors = cv,
+                target_modules = target_layers,
+                batch_size = bs,
+                drillers = drillers,
+                n_threads = n_threads,
+                verbose = verbose
+                )
