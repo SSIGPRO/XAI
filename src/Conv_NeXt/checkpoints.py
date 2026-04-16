6c29ec81e2990762d0e7deda97030c2ce3c69aa3
Arsharathi1 <arsharathi56@gmail.com>
2026-04-16 14:49:26 +0200

    Removed unnecessary files

--- src/Conv_NeXt/checkpoints.py
+++
@@ -1,6 +0,0 @@
-basic_dir = /home/arshakumari/repos/XAI/src/Conv_NeXt/checkpoints.py.resolve()
-tune_dir = basic_dir / "checkpoints" / run_id
-
-tune_dir.mkdir(parents=True, exist_ok=True)
-
-print("Saving checkpoints to:", tune_dir)
